efa77bacbe26d74cd117e9b20fea3b07cc77f5c8
thatfoogabriel <sotogabriel1228@gmail.com>
2023-06-24 11:47:45 -0700

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
-function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+export default function App() {
   return (
-    <div className="App">
-      <p>{!data ? "naw still broken :/" : data}</p>
-      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1aJ5uRmHJPtI2KWD6vssBdCJfDXe1VVQ&ehbc=2E312F" width="640" height="480"></iframe>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
