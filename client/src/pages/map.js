18196b1a4d805084a68a81e22caf61dc126d1b71
thatfoogabriel <sotogabriel1228@gmail.com>
2023-06-24 12:04:42 -0700

    linting error fix

--- client/src/pages/map.js
+++ client/src/pages/map.js
@@ -12,7 +12,7 @@ export default function Map() {
     return (
     <div className="App">
         <p>{!data ? "naw still broken :/" : data}</p>
-        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1aJ5uRmHJPtI2KWD6vssBdCJfDXe1VVQ&ehbc=2E312F" width="640" height="480"></iframe>
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1aJ5uRmHJPtI2KWD6vssBdCJfDXe1VVQ&ehbc=2E312F" width="640" height="480" title='map'></iframe>
     </div>
     );
 }
