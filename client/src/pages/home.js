efdfbd38a16145952f7b541748fec43b564d8d0c
Shavleen <shavleen.singh05@gmail.com>
2023-06-24 13:13:34 -0700

    start of home

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,18 @@
+import  React from 'react';
+import './home.css';
+
+
 export default function Home(){
     return(
-        <div>
-            Home page
+        <div class = "homenav">
+            <a href="#home">Home </a>
+            <a href="#news">About us </a>
+            <a href="#contact">Videos </a>
+            <a href="#about">Resources </a>
         </div>
+        
+
+
+        
     )
 }
